96160b75f96423d6e24b157fad616ecf7e8a4b50
Swati Pandey <swati.pandey@xperforce.com>
2023-06-21 18:34:22 +0530

    seach flight

--- tests/Book_a_Flight.spec.js
+++ tests/Book_a_Flight.spec.js
@@ -1,13 +1,16 @@
 import {test,expect} from '@playwright/test'
 import { Book_Flight} from '../pages/Book_Flight.spec'
+import { Search_Flight } from '../pages/Flight_search.spec'
 
 test('Book a Flight',async({page})=>{
 
    // await page.pause()
-
+    
     await page.goto('https://www.akasaair.com/')
     await page.waitForURL('https://www.akasaair.com/')
     const Book_Flights = new Book_Flight(page)
     await Book_Flights.flightBooking()
+    const SearchFlight= new Search_Flight(page)
+    await SearchFlight.SelectFlight()
 
 })
